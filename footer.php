0d22cb1273e26c1f48d1af6420a0aad81f3fd5dd
Nitesh-ops <niteshsingh.ns44@gmail.com>
2021-06-12 20:30:03 +0530

    icons

--- footer.php
+++ footer.php
@@ -9,7 +9,8 @@
     <title>Footer With Address And Phones</title>
 
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
-    <link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
     <link rel="stylesheet" type="text/css" href="CSS/footer.css">  
 
     <script src="JS/sweetalert.js"></script>
